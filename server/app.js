d90984cde3834362be31b42b42fe63ec66e94358
Aidar Arzybekov Tashtanbekovich <aidararzybekovtashtanbekovich@MacBook-Air-Aidar.local>
2023-03-06 11:26:16 +0600

    build/midlleweare

--- server/app.js
+++ server/app.js
@@ -19,14 +19,15 @@ app.use(
 	})
 )
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-
 app.use('/api', require('./router/index'))
 
+const publicPath = path.join(__dirname, 'public')
+
+app.use(express.static(publicPath))
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(publicPath, 'index.html'))
+})
 const server = http.createServer(app)
 
 server.listen(PORT, () => console.log(`Start server in port ${PORT}`))
